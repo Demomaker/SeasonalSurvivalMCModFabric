7d9d3b2c809d5c2e7e5277a77be6775de5de5925
Mike Bédard <mike01bedard@gmail.com>
2024-09-07 17:18:34 -0400

    make everything snow when is winter

---
+++ src/main/java/net/demomaker/seasonalsurvival/mixin/SnowMixin.java
@@ -0,0 +1,37 @@
+package net.demomaker.seasonalsurvival.mixin;
+
+import net.demomaker.seasonalsurvival.SeasonalSurvival;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Biome.class)
+public class SnowMixin {
+  @Inject(method = "getTemperature", at = @At("HEAD"), cancellable = true)
+  private void snowOnly(BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
+    if(SeasonalSurvival.isWinter) {
+      cir.setReturnValue(0.01F);
+    }
+  }
+
+  @Mixin(targets = "net.minecraft.world.biome.Biome$Weather")
+  public static class WeatherMixin {
+    @Inject(method = "downfall", at = @At("HEAD"), cancellable = true)
+    private void showDownfall(CallbackInfoReturnable<Float> cir) {
+      if(SeasonalSurvival.isWinter) {
+        cir.setReturnValue(1.0f);
+      }
+    }
+
+    @Inject(method = "hasPrecipitation", at = @At("HEAD"), cancellable = true)
+    private void injectHasPrecipitation(CallbackInfoReturnable<Boolean> cir) {
+      // Add your condition for temporarily allowing rain particles
+      if (SeasonalSurvival.isWinter) {
+        cir.setReturnValue(true); // Override the result to true to show precipitation
+      }
+    }
+  }
+}
